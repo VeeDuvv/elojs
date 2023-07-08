fad3403c8f32144791f8d57da00aec5a2aa1618d
[Vamsi Duvvuri] <vamsi.duvvuri@icloud.com>
2023-07-08 14:45:30 -0400

    cldnt fix the error (yet!) on last ex on pg. 85

--- 000scratchpad.js
+++ 000scratchpad.js
@@ -1,26 +1,55 @@
 // this file is for testing and debugging purposes only
 // it contains sample code that is not part of the main application / exercises
 
-function sum(range) {
-    let total = 0;
-    for (let value of range) {
-        total += value;
-    }
-    return total;
-}
 
-function range(start, end, step = 1) {
-    let range = [];
-    if (step > 0) {
-        for (let i = start; i <= end; i += step) {
-            range.push(i);
-        }
-    } else {
-        for (let i = start; i >= end; i += step) {
-            range.push(i);
-        }
-    }
-    return range;
-}
+/*
+// Page 85 // 
+*/
 
-console.log(sum(range(1, 10)));
+// function sum(range) {
+//     let total = 0;
+//     for (let value of range) {
+//         total += value;
+//     }
+//     return total;
+// }
+
+// function range(start, end, step = 1) {
+//     let range = [];
+//     if (step > 0) {
+//         for (let i = start; i <= end; i += step) {
+//             range.push(i);
+//         }
+//     } else {
+//         for (let i = start; i >= end; i += step) {
+//             range.push(i);
+//         }
+//     }
+//     return range;
+// }
+
+// console.log("sum of numbers 1-10 is: "+sum(range(1, 10)));
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// function repeatLog(n) {
+//     for (let i = 0; i < n; i++) {
+//         console.log(i);
+//     }
+// }
+
+// repeatLog(3);
+
+// function repeat(n, action) {
+//     for (let i = 0; i < n; i++) {
+//         action("repeat(n) "+i);
+//     }
+// }
+// repeat(5, console.log);
+
+// let labels = [];
+// repeat(5, i => {labels.push(`Unit ${i + 1}`)});
+// console.log(labels);
+
